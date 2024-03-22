29449cf8ba897a4742b02430bf6a1c657e26f437
E. Hannah <echannah631@gmail.com>
2024-03-21 22:03:00 -0500

    created the search function

--- src/api/types.ts
+++ src/api/types.ts
@@ -2,3 +2,20 @@ export type ViewerData = {
   name: string;
   avatarUrl: string;
 };
+
+export type SearchCriteria = {
+  org: string;
+  repo: string;
+};
+
+export type RepoData = {
+  repository: {
+    id: string;
+    name: string;
+    description: string;
+    viewerHasStarred: boolean;
+    stargazers: {
+      totalCount: number;
+    };
+  };
+};
